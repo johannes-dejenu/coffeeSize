1546cb84528c8ae4bb1cc7cefb14762e00b9c0b9
John <johannesdejenu3@gmail.noreply.github.com>
2026-09-01 21:57:02 +0300

    adding another behavior, which is the price field,  for coffeesize

--- Main.java
+++ Main.java
@@ -1,21 +1,23 @@
 public class Main{
     enum CoffeeSize{
-        SMALL(250),
-        MEDIUM(350),
-        LARGE(500);
+        SMALL(250, 25),
+        MEDIUM(350, 40),
+        LARGE(500, 50);
 
         private final int ml;
+        private final int price;
 
-        CoffeeSize(int ml) {
+        CoffeeSize(int ml, int price) {
             this.ml = ml;
+            this.price = price;
         }
 
         public int getMl(){
             return this.ml;
-        }
-
-        
-        
+        }  
+        public int getPrice(){
+            return price;
+        } 
     }
 
     public static void main(String[] args) {
