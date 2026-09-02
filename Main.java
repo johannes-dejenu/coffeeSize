40d5bc716b86237208b9c50e2fa913cc6a93080f
John <johannesdejenu3@gmail.noreply.github.com>
2026-09-02 03:04:53 +0300

    finalizing

--- Main.java
+++ Main.java
@@ -21,7 +21,11 @@ public class Main{
     }
 
     public static void main(String[] args) {
-        CoffeeSize size = CoffeeSize.MEDIUM;
-        System.out.println(size.getMl());
+        // CoffeeSize size = CoffeeSize.MEDIUM;
+        // System.out.println(size.getMl());
+
+        for (CoffeeSize size : CoffeeSize.values()){
+            System.out.println(size + " -> ml = " + size.getMl() + ", price = " + size.getPrice());
+        }
     }
 }
